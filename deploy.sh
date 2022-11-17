838d497d56f9b63be78d3119eb7ac0c417061d3c
andrey o <e.nosferatoss@gmail.com>
2022-11-17 14:46:40 -0500

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TAG=0.8
+docker build  --no-cache  -f ./deployment/docker/Dockerfile -t 667950714614.dkr.ecr.us-east-1.amazonaws.com/payment-service:$TAG .
+docker push 667950714614.dkr.ecr.us-east-1.amazonaws.com/payment-service:$TAG
+kubectl set image deployment/payment-service payment-service=667950714614.dkr.ecr.us-east-1.amazonaws.com/payment-service:$TAG
+kubectl rollout restart deployment/payment-service
+
